9d65b0f0d9f285004b3887a52b9d5864351e82d0
magumanakayama <domoido@icloud.com>
2025-07-15 23:00:46 +0900

    Appをチュートリアル的に変更、docker周りを微調整

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const User = ({ name }) => {
+  return <li style={{padding: '8px'}}>{name}</li>;
+};
+
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [users, setUsers] = useState(['maguma', 'nakayama', 'okabe']);
+
+  const userList = users.map((user, index) => {
+    return <User key={index} name={user}></User>;
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUsers([...users, inputText]);
+    console.log('handle submit');
+  };
+  
+  const handleChange = (event) => {
+    setInputText(event.target.value);
+    console.log('handle change: ', inputText);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {userList}
+      </ul>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type = "text" onChange={handleChange}/>
+        <button type="submit">追加</button>
+      </form>
+      <div>入力値：{inputText}</div>
     </div>
   );
 }
